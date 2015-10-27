09f88273be1021cd5621519e1c1743cd7b4af931
Colton <cbryant@mines.edu>
2015-10-27 11:34:51 -0600

    The eigenvalue solver works for the T matrix

--- oned_module.f90
+++ oned_module.f90
@@ -4,10 +4,11 @@ CONTAINS
 
 SUBROUTINE GaussLegendre(x, w, N)
   IMPLICIT NONE
-  DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: x,w,beta,Lambda_num_real, Lambda_denom_real,Lambda_num_im, Work
-  INTEGER :: N,i,j,Lwork,Info
-  DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: V, D, T, Eye
-  
+  DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: D,x,w,beta,Lambda,Work
+  INTEGER :: N,i,j,Lwork,Info,num_eig,Liwork
+  DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:,:) :: V, T, Eye
+  DOUBLE PRECISION :: abs_tol
+  INTEGER, ALLOCATABLE, DIMENSION(:) :: Isuppz,Iwork
   !BEGIN PROGRAM HERE
   ALLOCATE(beta(N))
   ALLOCATE(T(N,N))
@@ -31,30 +32,65 @@ SUBROUTINE GaussLegendre(x, w, N)
  !   END DO
  ! END DO
 
+  Lwork = 18*N+1
+  Liwork = 10*N+1
+  ALLOCATE(V(N,N), D(N), Lambda(N),Isuppz(2*N), Work(Lwork),&
+    Iwork(Liwork))
   !MAKE IDENTITY MATRIX
+  
   ALLOCATE(Eye(N,N))
   Eye(1:N,1:N) = 0
   DO i = 1,N
     Eye(i,i) = 1
+    D(i) = T(i,i)
   END DO
   
-  Lwork = -1
-
-  ALLOCATE(V(N,N), D(N,N), Lambda_num_real(N), Lambda_denom_real(N), Lambda_num_im(N), Work(1))
-  CALL SGEGV('N','V',N,T,N,Eye,N,Lambda_num_real,Lambda_num_im,Lambda_denom_real,V,N,V,N,Work,Lwork,Info)  
+  
+  
+  !CALL SGGEV('N','V',N,T,N,Eye,N,Lambda_num_real,Lambda_num_im,Lambda_denom_real,V,N,V,N,Work,Lwork,Info)  
   !DOES NOT WORK YET!
+  
+  
 
+  CALL DSTEGR('V','A',N, D, beta,0.d0,0.d0,0,0,abs_tol,num_eig,Lambda,V,N,Isuppz,Work,Lwork,Iwork,&
+    Liwork,Info) !WORKING!
+
+
+  !TEST STUFF
+  !DO i = 1, N
+  !  DO j=1,N
+  !    WRITE(*,*) T(i,j)
+  !  END DO
+  !END DO
   !Stores eigenvalues in x
-  DO i = 1,N
-    x(i) = Lambda_num_real(i)/Lambda_denom_real(i)
-  END DO
+  !DO i = 1,N
+  !  WRITE(*,*) Lambda(i), V(1:N,i)
+    
+  !END DO
   
   !NEED TO SORT EIGENVALUES AND TRACK CHANGES IN index
 
 
 
 
-  DEALLOCATE(beta,T)
+   DEALLOCATE(beta,T,V,D,Lambda,Isuppz,Work,Iwork)
+   !DEALLOCATE(beta) 
+   !WRITE(*,*) 'beta'
+   !DEALLOCATE(T)
+   !WRITE(*,*) 'T'
+   !DEALLOCATE(V)
+   !WRITE(*,*) 'V'
+   !DEALLOCATE(D)
+   !WRITE(*,*) 'D'
+   !DEALLOCATE(Lambda)
+   !WRITE(*,*) 'Lambda'
+   !DEALLOCATE(Isuppz)
+   !WRITE(*,*) 'Isuppz'
+   !DEALLOCATE(Work)
+   !WRITE(*,*) 'Work'
+   !DEALLOCATE(Iwork)
+   !WRITE(*,*) 'Iwork'
+
 
 
 END SUBROUTINE GaussLegendre
--- tester.f90
+++ tester.f90
@@ -2,9 +2,16 @@ PROGRAM tester
 USE oneD_module
 IMPLICIT NONE
 DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: x,w
-INTEGER :: N=4
+INTEGER :: N=1000,i
+
+ALLOCATE(x(N),w(N))
+
 
 CALL GaussLegendre(x,w,N)
+
+!DO i = 1,4
+!  WRITE(*,*) x(i)
+!END DO
 
 
 END PROGRAM tester
